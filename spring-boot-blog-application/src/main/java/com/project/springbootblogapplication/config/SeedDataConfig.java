9b8cbf2de9edfcf29fec8f9fc24d9e6228252bed
Ishita Das <ishita.das9038@gmail.com>
2024-03-18 12:17:12 +0530

    Commit all functionalities like add/edit/delete post with Spring Security in place

--- spring-boot-blog-application/src/main/java/com/project/springbootblogapplication/config/SeedDataConfig.java
+++ spring-boot-blog-application/src/main/java/com/project/springbootblogapplication/config/SeedDataConfig.java
@@ -8,7 +8,6 @@ import com.project.springbootblogapplication.services.PostService;
 import com.project.springbootblogapplication.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
